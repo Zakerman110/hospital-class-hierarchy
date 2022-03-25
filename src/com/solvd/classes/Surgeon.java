8fbba9d92dc79b8990d0c03eafdae3ba5a4df0d0
Artur Hostiuk <artur1100011@gmail.com>
2022-03-25 17:17:55 +0200

    added static getter

--- src/com/solvd/classes/Surgeon.java
+++ src/com/solvd/classes/Surgeon.java
@@ -4,11 +4,15 @@ import java.util.Date;
 
 public final class Surgeon extends Doctor {
 
-    public static int count;
+    private static int count;
 
     public Surgeon(String firstName, String lastName, String gender, Date birthDate, String address, String phone,
                   Hospital hospital, Date joined, String[] education, Department department, String specialty) {
         super(firstName, lastName, gender, birthDate, address, phone, hospital, joined, education, department, specialty);
         count++;
+    }
+
+    public static int getCount() {
+        return count;
     }
 }
