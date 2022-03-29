f5cc8f53eebd71012335a710575dabde6dc9619c
Artur Hostiuk <artur1100011@gmail.com>
2022-03-29 16:08:59 +0300

    added maven

--- src/com/solvd/Exceptions/IncorrectRangeException.java
+++
@@ -1,8 +0,0 @@
-package com.solvd.Exceptions;
-
-public class IncorrectRangeException extends Exception{
-
-    public IncorrectRangeException(String message) {
-        super(message);
-    }
-}
--- src/com/solvd/Exceptions/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.solvd.Exceptions;
-
-public class NotFoundException extends Exception{
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
