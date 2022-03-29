cd35ebea443c779e29f610d061fd3916b269e375
Artur Hostiuk <artur1100011@gmail.com>
2022-03-29 16:20:30 +0300

    added logger for Transport

--- src/main/java/com/solvd/hospital/Classes/Transport.java
+++ src/main/java/com/solvd/hospital/Classes/Transport.java
@@ -2,6 +2,8 @@ package com.solvd.hospital.Classes;
 
 import com.solvd.hospital.Exceptions.IncorrectRangeException;
 import com.solvd.hospital.Exceptions.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 import java.util.Map;
@@ -10,6 +12,7 @@ public class Transport extends Vehicle {
 
     private String model;
     private int year;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Transport.class);
 
     public Transport(int wheels, int seats, String licenseNumber, Map<Date, Double> fuelConsumptionPerDay, String model, int year) {
         super(wheels, seats, licenseNumber, fuelConsumptionPerDay);
@@ -35,8 +38,16 @@ public class Transport extends Vehicle {
 
     @Override
     public Map<Date, Double> getLastDaysConsumption(int lastNDays) throws NotFoundException, IncorrectRangeException {
-        if(getFuelConsumptionPerDay().isEmpty()) throw new NotFoundException("No fuel consumption data");
-        if(getFuelConsumptionPerDay().size() < lastNDays) throw new IncorrectRangeException("Too many days inserted");
+        if(getFuelConsumptionPerDay().isEmpty())
+        {
+            LOGGER.error("No fuel consumption data");
+            throw new NotFoundException("No fuel consumption data");
+        }
+        if(getFuelConsumptionPerDay().size() < lastNDays)
+        {
+            LOGGER.error("Too many days inserted");
+            throw new IncorrectRangeException("Too many days inserted");
+        }
         return null;
     }
 
